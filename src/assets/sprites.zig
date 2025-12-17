b211bf608955b90142dcd11c39e26f48933f27ec
Shane Poppleton <shane@bjja.com.au>
2025-12-17 15:50:27 +1000

    work on attract mode

--- src/assets/sprites.zig
+++ src/assets/sprites.zig
@@ -1,8 +1,27 @@
 const std = @import("std");
 const engine = @import("engine");
+const SpriteLayoutBuilder = engine.graphics.SpriteLayoutBuilder;
+const SpriteLayout = engine.graphics.SpriteLayout;
+const RotationSet = engine.graphics.RotationSet;
+const RotationFrame = engine.graphics.RotationFrame;
+const Texture = engine.types.Texture;
 
-/// Define sprite IDs for each sprite sheet
-pub const PlayerSpriteId = enum {
+pub const SpriteType = enum {
+    player,
+    player_alt,
+    boss,
+    boss_alt,
+    goei,
+    zako,
+    scorpion,
+    midori,
+    galaxian,
+    tombow,
+    momji,
+    enterprise,
+};
+
+pub const SpriteId = enum {
     rotation_180,
     rotation_165,
     rotation_150,
@@ -10,56 +29,90 @@ pub const PlayerSpriteId = enum {
     rotation_120,
     rotation_105,
     rotation_90,
+    idle_1,
+    idle_2,
 };
 
 pub const Sprites = struct {
     allocator: std.mem.Allocator,
 
-    player_layout: engine.graphics.SpriteLayout(PlayerSpriteId),
-    player_rotations: engine.graphics.RotationSet(PlayerSpriteId),
+    layouts: std.AutoHashMap(SpriteType, SpriteLayout(SpriteId)),
+    rotations: std.AutoHashMap(SpriteType, RotationSet(SpriteId)),
 
     const Self = @This();
 
     pub fn init(allocator: std.mem.Allocator, ctx: *engine.Context) !Self {
         const sprite_sheet = try ctx.assets.loadTexture("textures/spritesheet.png");
+        var layouts = std.AutoHashMap(SpriteType, SpriteLayout(SpriteId)).init(allocator);
+        var rotations = std.AutoHashMap(SpriteType, RotationSet(SpriteId)).init(allocator);
 
-        // === PLAYER SETUP ===
-        var player_builder = engine.graphics.SpriteLayoutBuilder(PlayerSpriteId).init(allocator, sprite_sheet);
-
-        try player_builder.addSprite(.rotation_180, 1 + (0 * (16 + 2)), 1, 16, 16);
-        try player_builder.addSprite(.rotation_165, 1 + (1 * (16 + 2)), 1, 16, 16);
-        try player_builder.addSprite(.rotation_150, 1 + (2 * (16 + 2)), 1, 16, 16);
-        try player_builder.addSprite(.rotation_135, 1 + (3 * (16 + 2)), 1, 16, 16);
-        try player_builder.addSprite(.rotation_120, 1 + (4 * (16 + 2)), 1, 16, 16);
-        try player_builder.addSprite(.rotation_105, 1 + (5 * (16 + 2)), 1, 16, 16);
-        try player_builder.addSprite(.rotation_90, 1 + (6 * (16 + 2)), 1, 16, 16);
-
-        const player_layout = player_builder.build();
-
-        const player_rot_frames = try allocator.alloc(engine.graphics.RotationFrame(PlayerSpriteId), 7);
-        player_rot_frames[0] = .{ .id = .rotation_180, .angle = 180.0 };
-        player_rot_frames[1] = .{ .id = .rotation_165, .angle = 165.0 };
-        player_rot_frames[2] = .{ .id = .rotation_150, .angle = 150.0 };
-        player_rot_frames[3] = .{ .id = .rotation_135, .angle = 135.0 };
-        player_rot_frames[4] = .{ .id = .rotation_120, .angle = 120.0 };
-        player_rot_frames[5] = .{ .id = .rotation_105, .angle = 105.0 };
-        player_rot_frames[6] = .{ .id = .rotation_90, .angle = 90.0 };
-
-        const player_rotations = engine.graphics.RotationSet(PlayerSpriteId){
-            .layout = player_layout,
-            .frames = player_rot_frames,
-            .allow_horizontal_flip = true,
-            .allow_vertical_flip = true,
-        };
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .player, 0);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .player_alt, 1);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .boss, 2);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .boss_alt, 3);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .goei, 4);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .zako, 5);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .scorpion, 6);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .midori, 7);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .galaxian, 8);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .tombow, 9);
+        // try initSprite(allocator, sprite_sheet, &layouts, &rotations, .momji, 10);
+        try initSprite(allocator, sprite_sheet, &layouts, &rotations, .enterprise, 11);
 
         return .{
             .allocator = allocator,
-            .player_layout = player_layout,
-            .player_rotations = player_rotations,
+            .layouts = layouts,
+            .rotations = rotations,
         };
     }
 
+    pub fn initSprite(
+        allocator: std.mem.Allocator,
+        texture: Texture,
+        layouts: *std.AutoHashMap(SpriteType, SpriteLayout(SpriteId)),
+        rotations: *std.AutoHashMap(SpriteType, RotationSet(SpriteId)),
+        sprite_type: SpriteType,
+        row: f32,
+    ) !void {
+        var builder = SpriteLayoutBuilder(SpriteId).init(allocator, texture);
+
+        try builder.addSprite(.rotation_180, 1 + (0 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+        try builder.addSprite(.rotation_165, 1 + (1 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+        try builder.addSprite(.rotation_150, 1 + (2 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+        try builder.addSprite(.rotation_135, 1 + (3 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+        try builder.addSprite(.rotation_120, 1 + (4 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+        try builder.addSprite(.rotation_105, 1 + (5 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+        try builder.addSprite(.rotation_90, 1 + (6 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+
+        try builder.addSprite(.idle_1, 1 + (6 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+        if (sprite_type == .boss or sprite_type == .boss_alt or sprite_type == .goei or sprite_type == .zako) try builder.addSprite(.idle_2, 1 + (7 * (16 + 2)), 1 + (row * (16 + 2)), 16, 16);
+
+        const layout = builder.build();
+
+        const rot_frames = try allocator.alloc(RotationFrame(SpriteId), 7);
+        rot_frames[0] = .{ .id = .rotation_180, .angle = 180.0 };
+        rot_frames[1] = .{ .id = .rotation_165, .angle = 165.0 };
+        rot_frames[2] = .{ .id = .rotation_150, .angle = 150.0 };
+        rot_frames[3] = .{ .id = .rotation_135, .angle = 135.0 };
+        rot_frames[4] = .{ .id = .rotation_120, .angle = 120.0 };
+        rot_frames[5] = .{ .id = .rotation_105, .angle = 105.0 };
+        rot_frames[6] = .{ .id = .rotation_90, .angle = 90.0 };
+
+        try layouts.put(sprite_type, layout);
+        try rotations.put(sprite_type, RotationSet(SpriteId){
+            .layout = layout,
+            .frames = rot_frames,
+            .allow_horizontal_flip = true,
+            .allow_vertical_flip = true,
+        });
+    }
+
     pub fn deinit(self: *Self) void {
-        self.allocator.free(self.player_rotations.frames);
+        var rot_iter = self.rotations.valueIterator();
+        while (rot_iter.next()) |rotation_set| {
+            self.allocator.free(rotation_set.frames);
+        }
+        self.rotations.deinit();
+        self.layouts.deinit();
     }
 };
--- src/modes/attract.zig
+++ src/modes/attract.zig
@@ -2,10 +2,13 @@ const std = @import("std");
 const engine = @import("engine");
 const GameMode = @import("../game_mode.zig").GameMode;
 const GameState = @import("../game_state.zig").GameState;
+const SpriteId = @import("../assets/sprites.zig").SpriteId;
 
 pub const Attract = struct {
     submode: SubMode,
     timer: f32,
+    wing_timer: f32 = 0.0,
+    sprite_id: SpriteId = .idle_1,
 
     const Self = @This();
 
@@ -17,6 +20,7 @@ pub const Attract = struct {
 
     pub fn update(self: *Self, ctx: *engine.Context, dt: f32) !?GameMode {
         self.timer += dt;
+        self.wing_timer += dt;
 
         // Potentially time out to a new mode
         if (self.timer < 5.0) {
@@ -26,6 +30,15 @@ pub const Attract = struct {
                 .game_info => .demo_gameplay,
                 .demo_gameplay => .high_score_table,
             };
+        }
+
+        if (self.wing_timer > 0.5) {
+            if (self.sprite_id == .idle_1) {
+                self.sprite_id = .idle_2;
+            } else {
+                self.sprite_id = .idle_1;
+            }
+            self.wing_timer = 0;
         }
 
         // Update current submode
@@ -69,16 +82,40 @@ pub const Attract = struct {
     }
 
     pub fn draw(self: *Self, ctx: *engine.Context, state: *GameState) !void {
-        _ = self;
         ctx.renderer.drawTextGridCentered("GALAGA", 4, engine.types.Color.sky_blue);
         ctx.renderer.drawTextGridCentered("--- SCORE ---", 6, engine.types.Color.sky_blue);
 
         ctx.renderer.drawText("50      100", .{ .x = 0.45, .y = 0.3 }, engine.types.Color.sky_blue);
         ctx.renderer.drawText("80      160", .{ .x = 0.45, .y = 0.36 }, engine.types.Color.sky_blue);
 
-        if (state.sprites.player_rotations.getSpriteForAngle(90.0)) |flipped| {
-            ctx.renderer.drawFlippedSprite(flipped, .{ .x = 0.3, .y = 0.315 });
-            ctx.renderer.drawFlippedSprite(flipped, .{ .x = 0.3, .y = 0.375 });
+        if (state.sprites.layouts.get(.goei)) |goei_layout| {
+            if (goei_layout.getSprite(self.sprite_id)) |sprite| {
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.3, .y = 0.317 });
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.65, .y = 0.677 });
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.73, .y = 0.677 });
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.85, .y = 0.677 });
+            }
+        }
+        if (state.sprites.layouts.get(.zako)) |zako_layout| {
+            if (zako_layout.getSprite(self.sprite_id)) |sprite| {
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.3, .y = 0.377 });
+            }
+        }
+
+        if (state.sprites.layouts.get(.boss)) |boss_layout| {
+            if (boss_layout.getSprite(self.sprite_id)) |sprite| {
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.5, .y = 0.497 });
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.2, .y = 0.617 });
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.4, .y = 0.617 });
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.6, .y = 0.617 });
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.8, .y = 0.617 });
+            }
+        }
+        if (state.sprites.layouts.get(.player)) |player_layout| {
+            if (player_layout.getSprite(.idle_1)) |sprite| {
+                ctx.renderer.drawSprite(sprite, .{ .x = 0.5, .y = 0.91 });
+                // ctx.renderer.drawSpriteAnchored(sprite, .{ .x = 0.0, .y = 1.0 }, .bottom_left);
+            }
         }
     }
 
